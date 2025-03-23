55e8f7575543206033cafb7bb08730c4f617a602
Flamex <105858527+Flamexyy@users.noreply.github.com>
2025-03-23 17:07:10 +0100

    test

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   images: {
     remotePatterns: [
       {
@@ -12,6 +11,8 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   output: "standalone",
+  env: {
+    NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN: process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN,
+    NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN: process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN,
+  },
 };
-
-module.exports = nextConfig;
